d5e5a90dabeb5af562a5046747468d529d569b55
danila <spook9400@mail.ru>
2025-10-12 18:15:00 +0300

    add commit 8

--- materials/admin.py
+++ materials/admin.py
@@ -1,31 +1,31 @@
-# from django.contrib import admin
-#
-# from materials.models import Material, Tag
-#
-#
-# @admin.register(Material)
-# class RecordAdmin(admin.ModelAdmin):
-#     list_display = (
-#         "id",
-#         "title",
-#         "views_count",
-#         "publicated",
-#     )
-#     list_filter = (
-#         "title",
-#         "publicated",
-#         "views_count",
-#     )
-#     search_fields = ("title",)
-#
-#
-# @admin.register(Tag)
-# class TagAdmin(admin.ModelAdmin):
-#     list_display = (
-#         "id",
-#         "title",
-#         "description",
-#         "record",
-#     )
-#     list_filter = ("record",)
-#     search_fields = ("title", "record")
+from django.contrib import admin
+
+from materials.models import Material, Tag
+
+
+@admin.register(Material)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "views_count",
+        "publicated",
+    )
+    list_filter = (
+        "title",
+        "publicated",
+        "views_count",
+    )
+    search_fields = ("title",)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "description",
+        "record",
+    )
+    list_filter = ("record",)
+    search_fields = ("title", "record")
